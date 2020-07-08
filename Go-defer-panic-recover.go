c18a83b9284c1c6d423b0ce57823bb6f70ce1a34
qichaoyang <chaoyang.qi@mybogames.com>
2020-07-08 15:07:21 +0800

    xxx

--- Go-defer-panic-recover.go
+++ Go-defer-panic-recover.go
@@ -5,27 +5,27 @@ import "fmt"
 func main() {
 	ff()
 	//这一句肯定是最后执行，因为最后才到main函数
-	fmt.Println("Returned normally from f.")
+	fmt.Println("Returned normally from f.") //12
 }
 
 func ff() {
 	defer func() {
 		if r := recover(); r != nil {
-			fmt.Println("Recovered in f", r)
+			fmt.Println("Recovered in f", r) // 11
 		}
 	}()
-	fmt.Println("Calling g.")
+	fmt.Println("Calling g.") // 1
 	gg(0)
-	fmt.Println("Returned normally from g.")
+	fmt.Println("Returned normally from g.") //这句其实永远不会执行，出错之后，就走不到下面
 }
 
 func gg(i int) {
 	if i > 3 {
-		fmt.Println("Panicking!")
+		fmt.Println("Panicking!") //6
 		panic(fmt.Sprintf("%v", i))
 	}
-	defer fmt.Println("Defer in g", i)
-	fmt.Println("Printing in g", i)
+	defer fmt.Println("Defer in g", i) //7，8，9，10
+	fmt.Println("Printing in g", i) //2 ，3，4 ，5
 	gg(i + 1)
 }
 //Calling g.
