f88836f09f567e739ab371c227037e125bddc5eb
qichaoyang <chaoyang.qi@mybogames.com>
2021-08-27 17:27:57 +0800

    海外

---
+++ 数据结构+算法/三数之和为0.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func threeSum(nums []int)[][]int{
+	sort.Ints(nums)
+	result, start, end, index, addNum, length := make([][]int, 0), 0, 0, 0, 0, len(nums)
+	for index = 1; index < length-1; index++ {
+		start, end = 0, length-1
+		if index > 1 && nums[index] == nums[index-1] {
+			start = index - 1
+		}
+		for start < index && end > index {
+			if start > 0 && nums[start] == nums[start-1] {
+				start++
+				continue
+			}
+			if end < length-1 && nums[end] == nums[end+1] {
+				end--
+				continue
+			}
+			addNum = nums[start] + nums[end] + nums[index]
+			if addNum == 0 {
+				result = append(result, []int{nums[start], nums[index], nums[end]})
+				start++
+				end--
+			} else if addNum > 0 {
+				end--
+			} else {
+				start++
+			}
+		}
+	}
+	return result
+}
+func threeSum2(nums []int) [][]int {
+	//先对数组排序
+	sort.Ints(nums)
+	result := [][]int{}
+	for i := 0; i < len(nums)-1; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		j := i + 1
+		z := len(nums) - 1
+		for z > j {
+			b := nums[j]
+			c := nums[z]
+			if nums[i]+b+c > 0 {
+				z--
+			} else if nums[i]+b+c < 0 {
+				j++
+			} else {
+				item := []int{nums[i], b, c}
+				result = append(result, item)
+				for j < z && nums[j] == nums[j+1] {
+					j++
+				}
+				for j < z && nums[z] == nums[z-1] {
+					z--
+				}
+				j++
+				z--
+			}
+		}
+	}
+	return result
+}
+func main() {
+	nums := []int{1,-1,0,2,-1,-1}
+	fmt.Println(threeSum(nums))
+	fmt.Println(threeSum2(nums))
+}
