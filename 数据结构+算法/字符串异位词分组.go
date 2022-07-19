66f7b8f611196fcf2111cfb1ab8f9f8584a7e3fe
qichaoyang <chaoyang.qi@mybogames.com>
2022-07-19 09:49:02 +0800

    海外

--- 数据结构+算法/字符串异位词分组.go
+++ 数据结构+算法/字符串异位词分组.go
@@ -99,7 +99,9 @@ func groupAnagrams4(strs []string) [][]string {
 	mp := map[string][]string{}
 	for _, str := range strs {
 		s := []byte(str)
-		sort.Slice(s, func(i, j int) bool { return s[i] < s[j] })
+		sort.Slice(s, func(i, j int) bool {
+			return s[i] < s[j]
+		})
 		sortedStr := string(s)
 		//把排序过后，字母一样的字符串存储到一起，类似于
 		//map[abt:[bat] aet:[eat tea ate] ant:[tan nat]]
--- 数据结构+算法/快速排序.go
+++ 数据结构+算法/快速排序.go
@@ -4,31 +4,57 @@ import "fmt"
 
 func quickSort(arr []int, start, end int) {
 	if start < end {
-		i, j := start, end
-		key := arr[(start+end)/2]
-		for i <= j {
-			for arr[i] < key {
-				i++
+		left, right := start, end
+		mid := arr[(start+end)/2]
+		for left <= right {
+			for arr[left] < mid {
+				left++
 			}
-			for arr[j] > key {
-				j--
+			for arr[right] > mid {
+				right--
 			}
-			if i <= j {
-				arr[i], arr[j] = arr[j], arr[i]
-				i++
-				j--
+			if left <= right {
+				arr[left], arr[right] = arr[right], arr[left]
+				left++
+				right--
 			}
 		}
 
-		if start < j {
-			quickSort(arr, start, j)
+		if start < right {
+			quickSort(arr, start, right)
 		}
-		if end > i {
-			quickSort(arr, i, end)
+		if end > left {
+			quickSort(arr, left, end)
 		}
 	}
 }
+func quick(arr []int,start,end int)  {
+	for start<end{
+		left := start
+		right := end
+		mid := arr[(left+end)/2]
+		for left <= end{
+			if arr[left] < mid{
+				left++
+			}
+			if arr[end] > mid{
+				end--
+			}
+			if left <= end{
+				arr[left],arr[end] = arr[end],arr[left]
+				left++
+				end--
+			}
+		}
+		if start < right{
+			quick(arr,start,right)
+		}
+		if end > left{
+			quick(arr,left,end)
+		}
 
+	}
+}
 func main() {
 	arr := []int{3, 7, 9, 8, 38, 93, 12, 222, 45, 93, 23, 84, 65, 2}
 	quickSort(arr, 0, len(arr)-1)
