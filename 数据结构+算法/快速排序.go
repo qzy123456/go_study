c5de767c571c8175cc8443ed322b7163e45470fc
qichaoyang <chaoyang.qi@mybogames.com>
2022-07-20 16:37:59 +0800

    海外

--- 数据结构+算法/快速排序.go
+++ 数据结构+算法/快速排序.go
@@ -28,33 +28,7 @@ func quickSort(arr []int, start, end int) {
 		}
 	}
 }
-func quick(arr []int,start,end int)  {
-	for start<end{
-		left := start
-		right := end
-		mid := arr[(left+end)/2]
-		for left <= end{
-			if arr[left] < mid{
-				left++
-			}
-			if arr[end] > mid{
-				end--
-			}
-			if left <= end{
-				arr[left],arr[end] = arr[end],arr[left]
-				left++
-				end--
-			}
-		}
-		if start < right{
-			quick(arr,start,right)
-		}
-		if end > left{
-			quick(arr,left,end)
-		}
 
-	}
-}
 func main() {
 	arr := []int{3, 7, 9, 8, 38, 93, 12, 222, 45, 93, 23, 84, 65, 2}
 	quickSort(arr, 0, len(arr)-1)
