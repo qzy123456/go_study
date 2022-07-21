6257c483aa149e55736f3da74a32f2c274f8059e
qichaoyang <chaoyang.qi@mybogames.com>
2022-07-21 16:30:30 +0800

    有效的字母异位词

--- 数据结构+算法/有效的字母异位词.go
+++ 数据结构+算法/有效的字母异位词.go
@@ -2,25 +2,35 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 // 用两个字典分别把两个字符串的字符出现个数保存起来
 func isAnagram1(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
 	var sMap = make(map[rune]int)
 	var tMap = make(map[rune]int)
 
 	for _, c := range s {
-		sMap[c] +=  1
+		sMap[c] += 1
 	}
 	for _, c := range t {
-		tMap[c] +=  1
+		tMap[c] += 1
 	}
-	return reflect.DeepEqual(sMap, tMap)
+	for i, v := range sMap {
+		vv, ok := tMap[i]
+		//查不到，或者数量不一样
+		if !ok || vv != v {
+			return false
+		}
+	}
+	return true
+	//return reflect.DeepEqual(sMap, tMap)
 }
 
 func main() {
 	s := "anagram"
 	t := "nagaram"
-	fmt.Println(isAnagram1(s,t))
+	fmt.Println(isAnagram1(s, t))
 }
