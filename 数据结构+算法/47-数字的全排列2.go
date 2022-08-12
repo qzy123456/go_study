aecbf38f79d166642aed55f2455d34d2e1b221ae
qichaoyang <chaoyang.qi@mybogames.com>
2022-08-12 11:32:21 +0800

    xxa

--- 数据结构+算法/47-数字的全排列2.go
+++ 数据结构+算法/47-数字的全排列2.go
@@ -37,9 +37,42 @@ func generatePermutation47(nums []int, index int, p []int, res *[][]int, used *[
 	}
 	return
 }
+//46题的解法
+func permuteUnique2(nums []int) [][]int {
+	var res, path = make([][]int, 0), make([]int, 0)
+	var used = make([]bool, len(nums))
 
+	var dfs func()
+	dfs = func() {
+		if len(path) == len(nums) {
+			var temp = make([]int, len(path))
+			copy(temp, path)
+			res = append(res, temp)
+			return
+		}
+
+		for i:=0;i<len(nums);i++ {
+			if used[i] {
+				continue
+			}
+			//关键在这一句
+			if i != 0 && nums[i] == nums[i-1] && used[i-1] == false {
+				continue
+			}
+			path = append(path, nums[i])
+			used[i] = true
+			dfs()
+			// 回溯的过程中，将当前的节点从 path 中删除
+			path = path[:len(path) - 1]
+			used[i] = false
+		}
+	}
+	dfs()
+	return res
+}
 
 func main() {
 	nums := []int{1,1,3}
 	fmt.Println(permuteUnique(nums))
+	fmt.Println(permuteUnique2(nums))
 }
