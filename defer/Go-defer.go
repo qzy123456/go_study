fb5af39d2b0ea357a64abe2b10920bc072f9f44b
qichaoyang <chaoyang.qi@mybogames.com>
2022-09-19 14:16:32 +0800

    xxa

--- defer/Go-defer.go
+++ defer/Go-defer.go
@@ -6,6 +6,9 @@ func main() {
 	fmt.Println(f1())  //1
 	fmt.Println(f2())  //5
 	fmt.Println(f3())  //1
+	f44()
+	//a defer2: 2
+	//a defer1: 3
 }
 
 func f1() (result int) {
@@ -26,4 +29,18 @@ func f3() (r int) {
 		r = r + 5
 	}(r)
 	return 1
+}
+func f44() {
+	i := 0
+	//压栈，
+	defer func() {//defer1
+		i++//2+1
+		fmt.Println("a defer1:", i)//i=3
+	}()
+	//defer压栈，先计算这个
+	defer func() {//defer2
+		i++//1+1
+		fmt.Println("a defer2:", i)//i=2
+	}()
+	i++//i=1
 }
