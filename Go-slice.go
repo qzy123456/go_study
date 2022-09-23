1f9592283950157d271b5e7a9547c5fbc005ed74
qichaoyang <chaoyang.qi@mybogames.com>
2022-09-23 17:49:28 +0800

    xxa

--- Go-slice.go
+++ Go-slice.go
@@ -49,5 +49,25 @@ func main() {
 	fmt.Println("slice: ", slice) // slice:  [50 51 62 33]
 	fmt.Println("slice1: ", slice1) // slice1:  [50 51 62 33 35]
 	fmt.Println("slice2: ", slice2) // slice2:  [20 21 32 63 35 16]
+	s := []int{5}
+	s = append(s, 7)
+	s = append(s, 9)
+	fmt.Println(len(s),cap(s))
+	x := append(s, 11)
+	fmt.Println(len(x),cap(x))
+	y := append(s, 12)
+	fmt.Println(len(y),cap(y))
+	fmt.Println(s, x, y)
+	//[5 7 9] [5 7 9 12] [5 7 9 12]
+	//s := []int{5}       s 只有一个元素，[5]
+	//s = append(s, 7)    s 扩容，容量变为2，[5, 7]
+	//s = append(s, 9)    s 扩容，容量变为4，[5, 7, 9]。注意，这时 s 长度是3，只有3个元素
+	//x := append(s, 11)  由于 s 的底层数组仍然有空间，因此并不会扩容。这样，底层数组就变成了 [5, 7, 9, 11]。
+						  //注意，此时 s = [5, 7, 9]，容量为4；x = [5, 7, 9, 11]，容量为4。这里 s 不变
+	//y := append(s, 12)  这里还是在 s 元素的尾部追加元素，由于 s 的长度为3，容量为4，所以直接在底层数组索引为3的地方填上12。
+	   				      //结果：s = [5, 7, 9]，y = [5, 7, 9, 12]，x = [5, 7, 9, 12]，x，y 的长度均为4，容量也均为4
+	//所以最后程序的执行结果是：
+	//[5 7 9] [5 7 9 12] [5 7 9 12]
+	//这里要注意的是，append函数执行完后，返回的是一个全新的 slice，并且对传入的 slice 并不影响。
 }
 
