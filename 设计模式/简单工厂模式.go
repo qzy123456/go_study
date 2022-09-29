947156b063e2a0a1a4613e6bbc0d2e013a43dc14
qichaoyang <chaoyang.qi@mybogames.com>
2022-09-29 14:29:09 +0800

    xxa

---
+++ 设计模式/简单工厂模式.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+//API is interface
+type API interface {
+	Say(name string) string
+}
+
+//NewAPI return Api instance by type
+func NewAPI(t int) API {
+	if t == 1 {
+		return &hiAPI{}
+	} else if t == 2 {
+		return &helloAPI{}
+	}
+	return nil
+}
+
+//hiAPI is one of API implement
+type hiAPI struct{}
+
+//Say hi to name
+func (*hiAPI) Say(name string) string {
+	return fmt.Sprintf("Hi, %s", name)
+}
+
+//HelloAPI is another API implement
+type helloAPI struct{}
+
+//Say hello to name
+func (*helloAPI) Say(name string) string {
+	return fmt.Sprintf("Hello, %s", name)
+}
+
+func main() {
+	fmt.Println(NewAPI(1).Say("1"))
+	fmt.Println(NewAPI(2).Say("2"))
+}
