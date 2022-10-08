7c6679106c62fd1b95eee457f1a9c3c4566534bf
qichaoyang <chaoyang.qi@mybogames.com>
2022-10-08 11:58:00 +0800

    xxa

---
+++ Go-命名类型-非命名类型.go
@@ -0,0 +1,16 @@
+package main
+
+//具有名称的类型：例如 int、int64、float32、string、bool 等，这些都是预先声明的。
+//
+//另外，使用 type 关键字声明的任意类型也称为命名类型。
+//
+//var i int // named type
+//type myInt int // named type
+//var b bool // named type
+//命名 ( 定义 ) 类型总是与任何其他类型不同。
+//未命名类型
+//复合类型，包括 array、struct、pointer、function、interface、slice、Map 和 channel，都是未命名类型。
+//
+//[]string // unnamed type
+//map[string]string // unnamed type
+//[10]int // unnamed type
