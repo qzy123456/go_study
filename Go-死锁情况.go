77c5c24fa7f60df1c6cc9e3398dcac466b1aa008
qichaoyang <chaoyang.qi@mybogames.com>
2022-11-03 17:07:57 +0800

    189

--- Go-死锁情况.go
+++ Go-死锁情况.go
@@ -6,7 +6,7 @@ import (
 )
 
 var l sync.RWMutex
-
+//读锁的时候无需等待读锁的结束
 func lockAndRead() { // 可读锁内使用可读锁
 	l.RLock()
 	defer l.RUnlock()
@@ -14,7 +14,7 @@ func lockAndRead() { // 可读锁内使用可读锁
 	l.RLock()
 	defer l.RUnlock()
 }
-
+//写锁的时候要等待写锁的结束,panic
 func lockAndRead1() { // 全局锁内使用全局锁
 	l.Lock()
 	defer l.Unlock()
@@ -22,7 +22,7 @@ func lockAndRead1() { // 全局锁内使用全局锁
 	l.Lock()
 	defer l.Unlock()
 }
-
+//写锁的时候要等待读锁的结束,panic
 func lockAndRead2() { // 全局锁内使用可读锁
 	l.Lock()
 	defer l.Unlock() // 由于 defer 是栈式执行，所以这两个锁是嵌套结构
@@ -30,7 +30,7 @@ func lockAndRead2() { // 全局锁内使用可读锁
 	l.RLock()
 	defer l.RUnlock()
 }
-
+//读锁的时候要等待写锁的结束，panic
 func lockAndRead3() { // 可读锁内使用全局锁
 	l.RLock()
 	defer l.RUnlock()
